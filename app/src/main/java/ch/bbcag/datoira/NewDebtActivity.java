bb6d2293a7fd9f7b1578ebffa2f8825d0c4b6399
Luca Hänni <bhaenl@bbcnet.ch>
2017-05-02 13:45:10 +0200

    NewDebtAcitivy updated

--- app/src/main/java/ch/bbcag/datoira/NewDebtActivity.java
+++ app/src/main/java/ch/bbcag/datoira/NewDebtActivity.java
@@ -4,20 +4,34 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class NewDebtActivity extends AppCompatActivity {
+
+    private String amount;
+    private String lastname;
+    private String firstname;
+    private String tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_debt);
 
-        DatabaseContract.DatabaseHelper dbHelper = new DatabaseContract.DatabaseHelper(this);
+        EditText amountEditText = (EditText) findViewById(R.id.new_debt_amount_edit_text);
+        EditText lastnameEditText = (EditText) findViewById(R.id.new_debt_lastname_edit_text);
+        EditText firstnameEditText = (EditText) findViewById(R.id.new_debt_firstname_edit_text);
+        EditText telEditText = (EditText) findViewById(R.id.new_debt_tel_edit_text);
 
-        SQLiteDatabase db  = dbHelper.getWritableDatabase();
+        amount = amountEditText.getText().toString();
+        lastname = lastnameEditText.getText().toString();
+        firstname = firstnameEditText.getText().toString();
+        tel = telEditText.getText().toString();
 
-        ContentValues values = new ContentValues();
-
+        
 
     }
 }
